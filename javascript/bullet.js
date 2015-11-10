87ca364441db48d6c5a9168df09449e13c533b17
mo buksh (laptop) <mo.buksh@gmail.com>
2015-11-10 17:49:42 +1100

    bullets fire but not properly yet

--- javascript/bullet.js
+++ javascript/bullet.js
@@ -4,8 +4,7 @@
 
 var BULLET_SPEED = 500;
 
-var Bullet = function ()
-{
+var Bullet = function () {
     this.image = document.createElement("img"),
     this.image.src = "images/bullet.png";
     this.x = 0;
@@ -13,32 +12,35 @@ var Bullet = function ()
     this.velocity_x = 0;
     this.velocity_y = 0;
     this.dead = true;
+    this.exited_canvas = false;
 }
 
-Bullet.prototype.update = function (deltaTime)
-{
+Bullet.prototype.fire = function(origin_x, origin_y, dir_x, dir_y) {
+    //console.trace();
+    if (this.dead === true) {
+        this.x = origin_x;
+        this.y = origin_y;
+        this.velocity_x = dir_x;
+        this.velocity_y = dir_y;
+        this.dead = false;
+    }
+    //return "fired";
+}
+
+Bullet.prototype.update = function (deltaTime) {
     if (!this.dead) {
         this.x += this.velocity_x * deltaTime * BULLET_SPEED;
         this.y += this.velocity_y * deltaTime * BULLET_SPEED;
 
         if(this.x < 0 || this.x > MAP.tw * TILE || this.y < 0 || this.y > MAP.th * TILE)
         {
+            this.exited_canvas = true;
             this.dead = true;
         }
-
     }
 }
 
-Bullet.prototype.fire = function(origin_x, origin_y, dir_x, dir_y){
-    this.x = origin_x;
-    this.y = origin_y;
-    this.velocity_x = dir_x;
-    this.velocity_y = dir_y;
-    this.dead = false;
-}
-
-Bullet.prototype.draw = function (cam_x, cam_y)
-{
+Bullet.prototype.draw = function (cam_x, cam_y) {
     if (!this.dead) {
        context.save();
         context.translate(this.x - cam_x,
--- javascript/main.js
+++ javascript/main.js
@@ -106,8 +106,11 @@ var music = new Howl(
 //UNCOMMENT FOR MUSIC, add new music first 
 //music.play();
 var jitter = Math.random() * 0.2 - 0.1;
+console.log(jitter);
+//bullet.fire(100, 100, 1, jitter);
 
-bullet.fire(100, 100, 1, jitter);
+towerShootCount = 0;
+
 
 //RUN
 function run()
@@ -127,16 +130,46 @@ function run()
 		context.fillText("MOUSE DOWN - POSITION: " + mouselistener.page_x + " / " + mouselistener.page_y, 5, 20);
 	}
 
-	// BULLETS
-	bullet.update(deltaTime);
-
-	//bullet.fire(mouselistener.page_x, mouselistener.page_y, 5, 5 );
-
-	bullet.draw(10,10);
 
 	//TOWERS
 	testTower.update();
 	testTower.draw(mouselistener.page_x, mouselistener.page_y);
+	if (testTower.allTowers.length > 0) {
+		//console.log("tower length:", testTower.allTowers.lengthj
+		for (var i = 0; i < testTower.allTowers.length; i++) {
+			console.log("#towers: ", testTower.allTowers.length);
+			console.log("index: ", i);
+			for (var j = 0; j < 50; j++) {
+
+				console.log(j);
+				var jitter = Math.random() * 0.2 - 0.1;
+				testTower.allTowers[i].thisTowerBullets[j].fire(testTower.allTowers[i].x, testTower.allTowers[i].y, 1, jitter);
+				testTower.allTowers[i].thisTowerBullets[j].update(deltaTime);
+				testTower.allTowers[i].thisTowerBullets[j].draw(10, 10);
+			}
+			/*
+			console.log("shoot count: ", towerShootCount);
+			var jitter = Math.random() * 0.2 - 0.1;
+			testTower.allTowers[i].thisTowerBullets[towerShootCount].fire(testTower.allTowers[i].x, testTower.allTowers[i].y, 1, jitter);
+			testTower.allTowers[i].thisTowerBullets[towerShootCount].update(deltaTime);
+			testTower.allTowers[i].thisTowerBullets[towerShootCount].draw(10, 10);
+
+			if (towerShootCount < 50) {
+				towerShootCount++;
+			} else {
+				towerShootCount = 0;
+			}
+			*/
+		}
+	}
+
+	// BULLETS
+	//bullet.update(deltaTime);
+
+	//bullet.fire(mouselistener.page_x, mouselistener.page_y, 5, 5 );
+
+	//bullet.draw(10,10);
+
 	
 }
 
