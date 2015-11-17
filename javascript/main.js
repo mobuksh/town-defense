7f38c277060f59b01485b2fce8ef289474aeacbf
mo buksh (laptop) <mo.buksh@gmail.com>
2015-11-17 20:44:54 +1100

    CONFLICT MERGE

--- javascript/main.js
+++ javascript/main.js
@@ -191,18 +191,18 @@ function run() {
     drawhud();
     //drawTowerInventory();
 
-    if (mouselistener.mouseDown == true) {
-        context.fillStyle = "#f00";
-        context.font = "14px Arial";
-
-        context.fillText("MOUSE DOWN - POSITION: " + mouselistener.page_x + " / " + mouselistener.page_y, 5, 40);
-        context.fillText("TILE POSITION: " + pixelToTile(mouselistener.page_x) + " / " + pixelToTile(mouselistener.page_y), 5, 60);
-
-        var mouse_x = pixelToTile(mouselistener.page_x);
-        var mouse_y = pixelToTile(mouselistener.page_y);
-
-        context.fillText("TILE POSITION IN PIXELS: " + tileToPixel(mouse_x) + " / " + tileToPixel(mouse_y), 5, 80);
-    }
+    //if (mouselistener.mouseDown == true) {
+    //    context.fillStyle = "#f00";
+    //    context.font = "14px Arial";
+    //
+    //    context.fillText("MOUSE DOWN - POSITION: " + mouselistener.page_x + " / " + mouselistener.page_y, 5, 40);
+    //    context.fillText("TILE POSITION: " + pixelToTile(mouselistener.page_x) + " / " + pixelToTile(mouselistener.page_y), 5, 60);
+    //
+    //    var mouse_x = pixelToTile(mouselistener.page_x);
+    //    var mouse_y = pixelToTile(mouselistener.page_y);
+    //
+    //    context.fillText("TILE POSITION IN PIXELS: " + tileToPixel(mouse_x) + " / " + tileToPixel(mouse_y), 5, 80);
+    //}
 
 
     //TOWERS
